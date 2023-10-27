d21b2282484cb5b61e98194c702d3c53e551baa1
Shanu sarkar <sanumts484@gmail.com>
2023-10-27 18:56:19 +0530

    first commit

---
+++ besicreact/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./Chai";
+function App() {
+  return (
+    <>
+    <h1>Jai shree ganesh</h1>
+    <Chai/>
+    </>
+  );
+}
+
+export default App;
---
+++ besicreact/src/Chai.js
@@ -0,0 +1,12 @@
+
+function Chai() {
+    return(
+        <>
+            <h1>Ram Ram</h1>
+            <p>Shiya s ram</p>
+            <span>Jai maa swarswati</span>
+        </>
+    )
+}
+
+export default Chai;
