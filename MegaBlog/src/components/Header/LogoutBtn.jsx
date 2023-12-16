3fb4eca47dc023ad1b350dba9b0053ff90683308
Shanu sarkar <sanumts484@gmail.com>
2023-12-16 19:33:48 +0530

    Add-Blog

---
+++ MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useDispatch } from  'react-redux'
+import authServise from '../../apperite/auth'
+import { logout } from '../../store/authSlice'
+
+function LogoutBtn() {
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        authServise.logout().then(() => {
+            dispatch(logout())
+        })
+    }
+  return (
+        <button
+        className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' 
+        onClick={logoutHandler}
+        >Logout</button>
+  )
+}
+
+export default LogoutBtn
+
+
---
+++ MegaBlog/src/components/index.js
@@ -0,0 +1,58 @@
+// import Header from "./Header/Header";
+// import Footer from "./Footer/Footer";
+// import Container from "./container/Container";
+// import Logo from "./Logo";
+// import LogoutBtn from "./Header/LogoutBtn";
+// import RTE from "./RTE";
+// import Signup from "./Signup";
+// import Login from "./Login";
+// // import AddPost from "./AddPost";
+// import PostForm from "./pot-form/PostForm";
+// import PostCard from "./PostCard"
+// import AuthLayout from "./AuthLayout"
+
+// export {
+//     Header,
+//     Footer,
+//     Container,
+//     Logo,
+//     LogoutBtn,
+//     RTE,
+//     Signup,
+//     Login,
+//     // AddPost,
+//     PostForm,
+//     PostCard,
+//     AuthLayout
+// }
+
+import Select from "./Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./pot-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
